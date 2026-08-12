1b48faff9b51611144aafdb0f3b05b871652dc91
Vitoria F. <206957068+Vitoria-F-0@users.noreply.github.com>
2026-08-11 22:38:24 -0300

    Teste Estrutura Botão Concluir e Excluir

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,33 @@
-const digitar = document.querySelector('#digitar')
+const digitar = document.getElementById('digitar')
 const botaoAdicionar =  document.querySelector ('.botao')
-const listaTarefas = document.querySelector ('#listaTarefas')
+const listaTarefas = document.getElementById ('listaTarefas')
 
 const lista = []
 
 function exibirTarefas () {
         listaTarefas.innerHTML = ""; 
         lista.forEach(elemento => {
-        listaTarefas.innerHTML += `<li>${elemento}</li>`;
-    });
+            let tarefa = document.createElement('li')
+            const botaoConcluir = document.createElement('button')
+            const botaoExcluir = document.createElement('button')
+            
+            tarefa.innerText = ` ${elemento}`
+            botaoConcluir.classList.add('botaoEstilizado');
+            botaoExcluir.classList.add('botaoEstilizado');
+            tarefa.appendChild(botaoConcluir)
+            tarefa.appendChild(botaoExcluir)
+            listaTarefas.appendChild(tarefa)
+    })
+
 }
 
 function adicionarTarefa() {
-    lista.push(digitar.value.trim())
-    digitar.value = "";
+    if (digitar.value.length != 0) {
+        lista.push(digitar.value.trim())
+        digitar.value = ""
+    }
 }
-
 botaoAdicionar.addEventListener('click', (event) => {
-    adicionarTarefa();
-    exibirTarefas();
+    adicionarTarefa()
+    exibirTarefas()
 }) 
